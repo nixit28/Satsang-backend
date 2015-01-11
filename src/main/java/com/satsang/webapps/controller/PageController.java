053f3d0ea59e1e9a35e36e42303c6016a2628ef6
Nixit Patel <nixit28@hotmail.com>
2015-01-10 19:57:24 -0800

    Change

---
+++ src/main/java/com/satsang/webapps/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.satsang.webapps.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by nixit on 1/10/15.
+ */
+
+@Controller
+public class PageController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String root(Model model) {
+        model.addAttribute("message", "Hello");
+        return "home";
+    }
+
+}
